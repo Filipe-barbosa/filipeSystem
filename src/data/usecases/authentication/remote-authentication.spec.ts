3286f542adcf5941c00f6c0343fa2cd0f049b149
filipe-barbosa <filipebarbosa94@hotmail.com>
2022-06-13 16:38:16 -0300

    test: ensure RemoteAuthentications calls HttpPostClient

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,6 +1,7 @@
 import { HttpPostClientSpy } from "../../test/mock-http-client";
 import { RemoteAuthentication } from "./remote-authentication";
 import { faker } from "@faker-js/faker";
+import { mockAuthentication } from "../../../domain/test/mock-authentication";
 // factory designer pater
 type SutTypes = {
   sut: RemoteAuthentication;
@@ -17,9 +18,18 @@ const makeSut = (url: string = faker.internet.url()): SutTypes => {
 
 describe("RemoteAuthentication", () => {
   test("should call http HttpPostClient with correct URL", async () => {
+    const authenticationParams = mockAuthentication();
     const url = faker.internet.url();
     const { sut, httpPostClientSpy } = makeSut(url);
-    await sut.auth();
+    await sut.auth(mockAuthentication());
     expect(httpPostClientSpy.url).toBe(url);
+  });
+
+  test("should call http HttpPostClient with correct body", async () => {
+    const authenticationParams = mockAuthentication();
+    const url = faker.internet.url();
+    const { sut, httpPostClientSpy } = makeSut(url);
+    await sut.auth(authenticationParams);
+    expect(httpPostClientSpy.body).toEqual(authenticationParams);
   });
 });
