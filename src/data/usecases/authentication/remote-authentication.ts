8b387517ed1ea091558260d422cb2fcc6a031cd7
filipe-barbosa <filipebarbosa94@hotmail.com>
2022-06-14 10:34:17 -0300

    feat: ensure Remoteauthentication throws InvalidCredentials

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,5 +1,7 @@
 import { HttpPostClient } from "@/data/protocols/http/http-post-client";
 import { AuthenticationParams } from "@/domain/usecases/authentication";
+import { HttpStatusCode } from "@/data/protocols/http/http-response";
+import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials-error";
 
 export class RemoteAuthentication {
   constructor(
@@ -7,6 +9,13 @@ export class RemoteAuthentication {
     private readonly httpPostClient: HttpPostClient
   ) {}
   async auth(params: AuthenticationParams): Promise<void> {
-    await this.httpPostClient.post({ url: this.url, body: params });
+    const httpResponse = await this.httpPostClient.post({
+      url: this.url,
+      body: params,
+    });
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.unauthorized:
+        throw new InvalidCredentialsError();
+    }
   }
 }
