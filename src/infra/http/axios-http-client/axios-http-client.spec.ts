e5e83467484d2037d9015ba4b01ca5d5b6b08bc8
filipe-barbosa <filipebarbosa94@hotmail.com>
2022-08-01 23:10:14 -0300

    refactor: move post request creation to a factory method

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,6 +1,7 @@
 import { AxiosHttpClient } from './axios-http-client';
 import axios from 'axios';
 import { faker } from '@faker-js/faker';
+import { HttpPostParams } from '@/data/protocols/http';
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
@@ -9,10 +10,16 @@ const makeSut = (): AxiosHttpClient => {
   return new AxiosHttpClient();
 };
 
+const mockPostRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.random.words,
+});
+
 describe('AxiosHttpCLiente', () => {
   test('Should call axios with correct URl and Verb', async () => {
+    const request = mockPostRequest();
     const sut = makeSut();
-    await sut.post({ url });
-    expect(mockedAxios.post).toBeCalledWith(url);
+    await sut.post(request);
+    expect(mockedAxios.post).toBeCalledWith(request.url);
   });
 });
