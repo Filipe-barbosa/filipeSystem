4a985dde6e0f1fef0040e7777fcd53edc45544f1
filipe-barbosa <filipebarbosa94@hotmail.com>
2022-09-02 14:44:23 -0300

    fix: move global styles to index

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router } from '@/presentation/components';
+import '@/presentation/styles/global.scss';
 
 const rootElement = document.getElementById('main');
 
